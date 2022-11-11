4c40611af45cd7806c8c1fd6c16bc92ef8b864f1
배지 <ttsbu@naver.com>
2022-11-11 19:23:21 +0900

    react todolist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import Hello from './Hello';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './components/TodoTemplate';
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoCreate from './components/TodoCreate';
+import { TodoProvider } from './TodoContext';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
 
 function App() {
   return (
-    <div>
-      <Hello />
-    </div>
+    <TodoProvider>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+    </TodoProvider>
   );
 }
 
